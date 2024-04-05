8e5d0d5702dd1e3818aea42420df47a3d8f1ec94
1cgul <cgulino03@gmail.com>
2024-04-05 00:25:19 -0400

    s

--- src/main/java/aydin/firebasedemospring2024/DemoApp.java
+++ src/main/java/aydin/firebasedemospring2024/DemoApp.java
@@ -38,7 +38,7 @@ public class DemoApp extends Application {
         fstore = contxtFirebase.firebase();
         fauth = FirebaseAuth.getInstance();
 
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("signup"), 600, 400);
         stage.setScene(scene);
         stage.show();
     }
